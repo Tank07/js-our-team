1d00dd352edf03cda864f7b53abdc38115665bd6
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-24 20:24:08 +0100

    Struttura base

---
+++ main.js
@@ -0,0 +1,49 @@
+// Per la pagina 'Chi siamo' del nostro sito aziendale, dobbiamo sviluppare la sezione "Il nostro team".
+// Definiamo un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Milestone 1: stampare in console l'elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
+// Milestone 2: stampare i dati all'interno di un contenitore nella pagina html in modo dinamico, creando per ciascun membro del team un elemento che conterrà i suoi dati.
+// BONUS: stilizziamo la sezione realizzando le card di ciascun membro del team, come nel mockup allegato.
+// Consigli del giorno:
+
+let ourTeam = [
+
+    {
+        'nome' : 'Pancrazio',
+        'ruolo' : 'disoccupa',
+        'foto' : 'ada'
+    },
+
+    {
+        'nome' : 'Pancrazio',
+        'ruolo' : 'disocupato',
+        'foto' : 'aa'
+    },
+
+    {
+        'nome' : 'Pancrazio',
+        'ruolo' : 'disocato',
+        'foto' : 'da'
+    },
+
+    {
+        'nome' : 'Panczio',
+        'ruolo' : 'dicupato',
+        'foto' : 'aa'
+    },
+
+    {
+        'nome' : 'Pancio',
+        'ruolo' : 'disocpato',
+        'foto' : 'aa'
+    },
+
+    {
+        'nome' : 'Prazio',
+        'ruolo' : 'soccupato',
+        'foto' : 'a'
+    }
+
+
+];
+
+console.log(ourTeam)
