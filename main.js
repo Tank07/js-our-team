fd9c48ab56d4337f845b15e286d24f0885205213
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-27 15:41:41 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -3,47 +3,68 @@
 // Milestone 1: stampare in console l'elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
 // Milestone 2: stampare i dati all'interno di un contenitore nella pagina html in modo dinamico, creando per ciascun membro del team un elemento che conterrà i suoi dati.
 // BONUS: stilizziamo la sezione realizzando le card di ciascun membro del team, come nel mockup allegato.
-// Consigli del giorno:
 
 let ourTeam = [
 
     {
-        'nome' : 'Pancrazio',
-        'ruolo' : 'disoccupa',
-        'foto' : 'ada'
+        'nome' : 'Wayne Barnett',
+        'ruolo' : 'dFounder & CEO',
+        'foto' : '1.jpg'
     },
 
     {
-        'nome' : 'Pancrazio',
-        'ruolo' : 'disocupato',
-        'foto' : 'aa'
+        'nome' : 'Angela Caroll',
+        'ruolo' : 'Chief Editor',
+        'foto' : '2.jpg'
     },
 
     {
-        'nome' : 'Pancrazio',
-        'ruolo' : 'disocato',
-        'foto' : 'da'
+        'nome' : 'Tancredi Carluccio',
+        'ruolo' : 'Disoccupato',
+        'foto' : '3.jpg'
     },
 
     {
-        'nome' : 'Panczio',
-        'ruolo' : 'dicupato',
-        'foto' : 'aa'
+        'nome' : 'Angela Lopez',
+        'ruolo' : 'Social Media Manager',
+        'foto' : '4.jpg'
     },
 
     {
-        'nome' : 'Pancio',
-        'ruolo' : 'disocpato',
-        'foto' : 'aa'
+        'nome' : 'Scott Estrada',
+        'ruolo' : 'Developer',
+        'foto' : '7.jpg'
     },
 
     {
-        'nome' : 'Prazio',
-        'ruolo' : 'soccupato',
-        'foto' : 'a'
+        'nome' : 'Pancrazio ',
+        'ruolo' : 'Nullafacente',
+        'foto' : '9.jpg'
     }
 
 
 ];
 
 console.log(ourTeam)
+
+
+for (let i = 0; i < ourTeam.length; i++) {
+    
+    for ( let key in ourTeam[i]) {
+        console.log(`${key} => ${ourTeam[i][key]}`)
+        
+    }
+    
+}
+
+for (let x = 0; x < ourTeam.length; x++) {
+    document.getElementById("esito").innerHTML += `
+    <div class="card m-3 col-6" style="width: 18rem;">
+    <img src="./images/${ourTeam[x]["foto"]}" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">${ourTeam[x]["nome"]}</h5>
+      <p class="card-text text-muted">${ourTeam[x]["ruolo"]}</p>
+    </div>
+  </div>`
+    
+}
